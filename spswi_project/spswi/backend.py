18456b93477d2db5e9b2df55b50ac9e0fbe784bd
MrLemon162 <connor.mchugh5@gmail.com>
2019-05-20 08:27:27 +1000

    Input Testing V0.2

--- spswi_project/spswi/backend.py
+++ spswi_project/spswi/backend.py
@@ -3,13 +3,19 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 import codecs
+import operator
+import datetime
+
+# Import variables from flask
+# from main.controllers import test
 
 # app credentials
-cid ="244ce08ff106437f8e7565c2e796f4e3" 
+cid = "244ce08ff106437f8e7565c2e796f4e3"
 secret = "6e652d745f094973a50ef8204b953828"
 username = ""
 redirect_url = "http://localhost:8888/callback/"
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 scope = """
 user-read-recently-played user-top-read user-library-modify user-library-read playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative user-read-email user-read-birthdate user-read-private
@@ -17,45 +23,78 @@ user-read-playback-state user-modify-playback-state user-read-currently-playing 
 """
 token = util.prompt_for_user_token(username, scope, cid, secret, redirect_url)
 if token:
-    sp = spotipy.Spotify(auth=token) # try authentication
+    sp = spotipy.Spotify(auth=token)  # try authentication
 else:
-    print("Token error for", username) # return error if token cannot be found
+    print("Token error for", username)  # return error if token cannot be found
+
 
 class Spotify_Scrape:
 
-    test_artist = 'Flume' # test search string
-    count = -1 # counter used for loops
-    artist_names = [] # list stores followed artists
-    artist_URI = [] # Unique identifier for artists in spotify API
-    album_URI = [] # Unique identifier for albums in spotify API
-    master_album = [] # master list of albums
-    master_tracks = [] # master list of tracks
+    test_artist = 'Flume'  # test search string
+    count = -1  # counter used for loops
+    artist_names = []  # list stores followed artists
+    artist_URI = []  # Unique identifier for artists in spotify API
+    album_URI = []  # Unique identifier for albums in spotify API
+    master_album = []  # master list of albums
+    album_data = []  # holds temp album data information for storage in master list
+    master_tracks = []  # master list of tracks
+    track_data = []  # holds temp track data information
 
     def __init__(self, user_token):
         self.sp = spotipy.Spotify(auth=user_token)
-        self.follows = sp.current_user_followed_artists(50) # pulls list of artists following
+        self.follows = sp.current_user_followed_artists(
+            50)  # pulls list of artists following
         self.num_artists = len(self.follows["artists"]["items"])
-    
+
     def __str__(self):
         return str(self.__class__)
 
-    def testSearch(self): # return test artist dictionary
+    def timeController(self, days_ago):
+        self.today = datetime.date.today()
+        self.time_ago = self.today - datetime.timedelta(days=days_ago)
+        self.time_ago = datetime.datetime.strptime(self.time_ago, "%d/%m/%Y")
+        return self.time_ago
+
+    def testSearch(self):  # return test artist dictionary
         self.results = sp.search(q='artist:' + self.test_artist, type='artist')
         return self.results["artists"]["items"][0]
 
-    def debugger(self): # returns followed artists to identify artist dictionary labels
-        return self.follows["artists"]["items"][0] # delete 0 for all artists
+    def debugger(self, variant):  # returns followed artists to identify artist dictionary labels
+        if variant == 'artist':
+            # delete 0 for all artists
+            return self.follows["artists"]["items"][0]
+        if variant == 'album':
+            self.artistURIs()
+            self.count = 0  # reset counter
+            for occurrence in range(self.num_artists):
+                self.entry = self.artist_URI[occurrence]
+                self.artist_albums = sp.artist_albums(
+                    self.entry, album_type='album', limit='50')
+                if occurrence == 4:
+                    try:
+                        for unique_album in range(len(self.artist_albums["items"])):
+                            # add ['release_date'] to test release date pull
+                            self.master_album.append(
+                                self.artist_albums["items"][unique_album]['release_date'])
+                    except IndexError:
+                        continue
+                elif occurrence == 5:
+                    break
+                else:
+                    continue
+            return self.master_album
 
-    def pullArtists(self): # returns a list of every artist you follow
-        self.count = -1 # reset counter
+    def pullArtists(self):  # returns a list of every artist you follow
+        self.count = -1  # reset counter
         for artist in range(self.num_artists):
             self.count += 1
-            self.artist_names.append(self.follows["artists"]["items"][self.count]["name"])
+            self.artist_names.append(
+                self.follows["artists"]["items"][self.count]["name"])
         self.artist_names.sort()
         return self.artist_names
 
-    def artistURIs(self): # returns list of artist URIs
-        self.count = -1 # reset counter
+    def artistURIs(self):  # returns list of artist URIs
+        self.count = -1  # reset counter
         # Fetch Artist URIs
         for artist in range(self.num_artists):
             self.count += 1
@@ -63,41 +102,61 @@ class Spotify_Scrape:
             self.artist_URI.append(self.uri)
         return self.artist_URI
 
-    def albumURIs(self):
+    def albumURIs(self, days_ago):
         self.artistURIs()
-        self.count = 0 # need count from 0 for this loop
+        self.count = -1  # reset counter
+        self.timeController(days_ago)
         # Fetch Album URIs
         for occurrence in range(self.num_artists):
             self.entry = self.artist_URI[occurrence]
-            self.artist_albums = sp.artist_albums(self.entry, album_type='album', limit='50')
+            self.artist_albums = sp.artist_albums(
+                self.entry, album_type='album', limit='50')
             try:
                 for unique_album in range(len(self.artist_albums["items"])):
-                    self.master_album.append(self.artist_albums["items"][unique_album]) #["uri"]
+                    self.date_con = self.artist_albums["items"][unique_album]["release_date"]
+                    self.date_con = datetime.datetime.strptime(
+                        self.date_con, "%d/%m/%Y")
+                    if self.time_ago > self.date_con:
+                        self.album_data.append(
+                            self.artist_albums["items"][unique_album]["uri"])
+                        self.album_data.append(
+                            self.artist_albums["items"][unique_album]["release_date"])
+                        self.master_album.append(self.album_data)
+                    self.album_data = []
             except IndexError:
                 continue
         return self.master_album
 
-    def albumTracks(self):
-        self.albumURIs()
+    def albumTracks(self, days_ago):
+        self.albumURIs(days_ago)
         for occurrence in range(len(self.master_album)):
-            self.unique_album_uri = self.master_album[occurrence]
+            self.unique_album_uri = self.master_album[occurrence][0]
+            self.release_date = self.master_album[occurrence][1]
             self.album_tracks = sp.album_tracks(self.unique_album_uri)
             try:
                 for unique_track in range(len(self.album_tracks["items"])):
-                    self.mastfer_tracks.append(self.album_tracks)
+                    self.track_data.append(
+                        self.album_tracks["items"][unique_track]["uri"])
+                    self.track_data.append(self.release_date)
+                    self.master_tracks.append(self.track_data)
+                    self.track_data = []
             except IndexError:
                 continue
+        self.master_tracks.sort(key=operator.itemgetter(1), reverse=True)
         with open("Output.txt", "w", encoding="utf-8") as text_file:
             print(f"{self.master_tracks}", file=text_file)
         print("Done!")
         return self.master_tracks
 
-#user = Spotify_Scrape(token)
 
-## print(user.testSearch()) # test search connection
-## print(user.debugger()) # test followed artists connection
-## print(user.pullArtists()) # prints list with followed artist names
-## print(user.artistURIs()) # prints list of artist URIs
-## print(user.albumURIs()) # returns list of artist URIs
-## print(user.albumTracks())
-## user.albumTracks()
+user = Spotify_Scrape(token)
+
+# print(user.testSearch()) # test search connection
+# print(user.debugger('artist')) # test followed artists connection
+# print(user.debugger('album'))
+# print(user.pullArtists()) # prints list with followed artist names
+# print(user.artistURIs()) # prints list of artist URIs
+# print(user.albumURIs(365)) # returns list of artist URIs
+# print(user.albumTracks())
+user.albumTracks(365)
+# print(user.timeController(365))
--- spswi_project/spswi/main/controllers.py
+++ spswi_project/spswi/main/controllers.py
@@ -1,19 +1,30 @@
 from flask import Blueprint, render_template, request
-from spswi.backend import pullArtists
+#from spswi.backend import pullArtists
 
 main = Blueprint('main', __name__)
 auth = Blueprint('auth', __name__)
 
+# Time period variables
+day = 1
+month = 30
+year = 365
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if 'login' in request.form:
+        frequency = 1 * year
+        days_ago = 1 * month
+
+        try:
+            frequency = int(request.form["frequency"])
+            print(frequency)
+        except:
             pass
-        if 'search_artist' in request.form:
-            pass
-        if 'followed_artist' in request.form:
-            print('working')
-            pullArtists()
+
+        # if 'frequency' in request.form:
+            
+        # if 'days_ago' in request.form:
+        #     pass
 
     return render_template('primary.html',)# pull_artist = pullArtists())
 
