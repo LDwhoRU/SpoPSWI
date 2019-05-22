33555d23446fc67c796fcb4cd5a366530ebb8b14
LDwhoRU <50721703+LDwhoRU@users.noreply.github.com>
2019-05-22 16:10:42 +1000

    Delety boi
    This update fixes delete function to delete all. Still adding duplicates every other run, will be fixed

--- backend.py
+++ backend.py
@@ -8,7 +8,7 @@ import random
 
 # filter dates
 today = datetime.date.today()
-time_ago = today - datetime.timedelta(days=10000)
+time_ago = today - datetime.timedelta(days=1000)
 print('Filtering from ' + str(time_ago))
 
 # app credentials
@@ -38,7 +38,6 @@ class Spotify_Scrape:
     master_album = [] # master list of albums
     album_data = [] # holds temp album data information for storage in master list
     master_tracks = [] # master list of tracks
-    playlist_uris = [] # stores URIs of tracks in playlist to delete
     playlist_names = [] # stores playlist names of user
     fixed_list = False # Restricts playlist to 100 songs
 
@@ -46,7 +45,7 @@ class Spotify_Scrape:
         self.sp = spotipy.Spotify(auth=user_token)
         self.follows = sp.current_user_followed_artists(50) # pulls list of artists following
         self.num_artists = len(self.follows["artists"]["items"])
-        playlist_id = self.checkPlaylists() # Spotify playlist ID to add tracks to
+        self.playlist_id = self.checkPlaylists() # Spotify playlist ID to add tracks to
     
     def __str__(self):
         return str(self.__class__)
@@ -159,6 +158,7 @@ class Spotify_Scrape:
                     self.result = sp.user_playlist_add_tracks(username, self.playlist_id, self.shuffled_ids)
                     self.shuffled_ids = []
                     del self.track_ids[0:100]
+                self.result = sp.user_playlist_add_tracks(username, self.playlist_id, self.track_ids)
         else:
             self.result = sp.user_playlist_add_tracks(username, self.playlist_id, self.track_ids)
         #print(self.track_ids) # Returns and prints list of track ids to add to playlist
@@ -166,10 +166,15 @@ class Spotify_Scrape:
 
     def playlistRemove(self):
         global username
-        self.result = sp.user_playlist(username, self.playlist_id)
-        for entry in range(len(self.result["tracks"]["items"])):
-            self.playlist_uris.append(self.result["tracks"]["items"][entry]['track']['uri'])
+        self.playlist_uris = [] # stores URIs of tracks in playlist to delete
+        self.result = sp.user_playlist_tracks(username, self.playlist_id)
+        for entry in range(len(self.result["items"])):
+            self.playlist_uris.append(self.result["items"][entry]['track']['uri'])
         self.remove_all = sp.user_playlist_remove_all_occurrences_of_tracks(username, self.playlist_id, self.playlist_uris) # Remove all tracks from playlist
+        # Rerun to be absolutely sure if more tracks found
+        self.result = sp.user_playlist_tracks(username, self.playlist_id)
+        if len(self.result["items"]) > 0:
+            self.playlistRemove()
 
     def checkPlaylists(self):
         self.userInfo()
